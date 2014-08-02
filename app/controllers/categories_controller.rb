d826635397b049d62da4473584cdd886f02c516e
jacobi007 <jk.kubiak@gmail.com>
2014-08-02 12:54:30 +0200

    add devise_for method in routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
-  
+
   expose(:categories)
   expose(:category)
   expose(:product) { Product.new }
@@ -43,5 +43,5 @@ class CategoriesController < ApplicationController
   private
     def category_params
       params.require(:category).permit(:name)
-    end
+    end      
 end
