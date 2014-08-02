e874352940577fa9f277921639246944e01bbc2e
jacobi007 <jk.kubiak@gmail.com>
2014-08-02 13:25:43 +0200

    complete step 4

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,17 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :description, :title, :price
+  validates_format_of :price, with: /\A\d+\.?\d{0,2}\z/
+
+  def average_rating
+    average = 0
+    reviews = self.reviews
+
+    reviews.each do |rev|
+      average += rev.rating
+    end
+    average /= reviews.count.to_f
+  end
 end
